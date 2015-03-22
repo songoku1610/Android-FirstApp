8c02406e0724668526e02dba284c68cce4c1a71a
Vu Xuan Tuyen <vuxuantuyen.mta@gmail.com>
2015-03-22 16:01:42 +0700

    working actionbar

--- app/src/main/java/com/example/vxtuyen/fistapp/DisplayMesageActivity.java
+++ app/src/main/java/com/example/vxtuyen/fistapp/DisplayMesageActivity.java
@@ -19,10 +19,13 @@ public class DisplayMesageActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+       TextView textView = new TextView(this);
+       textView.setTextSize(40);
+       textView.setText(message);
+        System.out.println("---------------------");
+        System.out.println(android.R.attr.actionBarSize);
         setContentView(textView);
+      //  setContentView(R.layout.activity_display_mesage);
     }
 
     @Override
